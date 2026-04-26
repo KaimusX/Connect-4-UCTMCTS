30d73cdac7c2cb499093b7b702d332f1c78a7485
Luis Franco <luisfranco.business@gmail.com>
2026-04-26 09:57:29 -0600

    File reading logic

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import sys
-
+import sys    
+        
 def main():
+    # Argc
     if len(sys.argv) != 4:
         print("Usage: python main.py <input_file> <Verbose||Brief||None> <parameter>")
         sys.exit(1)
 
+    # assign inputs
     filename = sys.argv[1]
     output_mode = sys.argv[2]
     parameter = int(sys.argv[3])
@@ -13,3 +15,10 @@ def main():
         print("Error: Output mode must be Verbose, Brief, or None")
         sys.exit(1)
 
+    # read file
+    with open(filename, "r") as file:
+        lines = [line.strip() for line in file.readlines() if line.strip()]
+
+    algorithm = lines[0]
+    player = lines[1]
+    board = [list(row) for row in lines[2:8]]
